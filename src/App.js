9eb3a41b67cb236592dbb1c67b8d405ea5d1605c
Vikas Sharma <vkassharma19492@gmail.com>
2022-11-24 01:34:05 +0530

    part1 has been updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Anecdote = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {props.ancd}
+      <br />
+      has {props.vot} votes
     </div>
-  );
+  )
+}
+
+const App = () => {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
+  const [ selected, setSelected ] = useState(0)
+
+  const [ vote, setVote ] = useState(new Uint8Array(anecdotes.length))
+
+  const randomAnecdotes = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const updateVote = () => {
+    const copy = [...vote]
+    copy[selected] += 1
+    setVote(copy)
+  }
+
+  const topVote = vote.indexOf(Math.max(...vote))
+
+  console.log(selected)
+  console.log(vote)
+  console.log(vote[topVote])
+  console.log(anecdotes[topVote])
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote ancd={anecdotes[selected]} vot={vote[selected]} />
+      <br />
+      <button onClick={updateVote}>vote</button><button onClick={randomAnecdotes}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <Anecdote ancd={anecdotes[topVote]} vot={vote[topVote]} />
+    </div>
+  )
 }
 
 export default App;
